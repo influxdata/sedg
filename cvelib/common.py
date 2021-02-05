7355f44ae7ca7a808b2202ba518b3c265ef0e49c
Jamie Strandboge <jamie@influxdata.com>
2021-02-05 16:26:12 -0600

    bin/cve-check-syntax: move config functions to cvelib/common.py
    TODO: unittests

--- cvelib/common.py
+++ cvelib/common.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
 
+import configparser
+import os
 import re
+import shutil
 import sys
+import tempfile
 
 # The CVE file format follows RFC6532 (UTF-8 of RFC5322)
 from email.parser import BytesHeaderParser
 
 # TODO: we want RFC6532
 from email.policy import default
+
+
+# Our config file
+configFilePath = os.path.expandvars("$HOME/.config/influx-security-tools.conf")
+
+# Compile common regex on import
+rePatterns = {
+    # CVE-YYYY-XXXX (1-12 X's)
+    # CVE-YYYY-NNNX (1-11 N's)
+    # CVE-YYYY-GHXXXX#AAAA (1-12 X's, 1-40 A's)
+    "CVE": re.compile(r"^CVE-[0-9]{4}-([0-9N]{3,11}[0-9]|GH[0-9]{1,12}#[a-z0-9+.-]{1,40})$"),
+    # date only: YYYY-MM-DD
+    # date and time: YYYY-MM-DD HH:MM:SS
+    # date and time with timezone: YYYY-MM-DD HH:MM:SS TZ|+-N
+    "date-only": re.compile(r"20[0-9][0-9]-[01][0-9]-[0-3][0-9]$"),
+    "date-time": re.compile(
+        r"20[0-9][0-9]-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]$"
+    ),
+    "date-full-offset": re.compile(
+        r"20[0-9][0-9]-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] [+-][01][0-9]+$"
+    ),
+    "date-full-tz": re.compile(
+        r"20[0-9][0-9]-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] [A-Z]+$"
+    ),
+    "date-full": re.compile(
+        r"20[0-9][0-9]-[01][0-9]-[0-3][0-9](| [0-2][0-9]:[0-5][0-9]:[0-5][0-9](| ([+-][01][0-9]+|[A-Z]+)))$"
+    ),
+}
 
 
 #
@@ -46,29 +78,37 @@ def readCveHeaders(fn):
         return BytesHeaderParser(policy=default).parse(fp)
 
 
-# Compile common regex on import
-rePatterns = {
-    # CVE-YYYY-XXXX (1-12 X's)
-    # CVE-YYYY-NNNX (1-11 N's)
-    # CVE-YYYY-GHXXXX#AAAA (1-12 X's, 1-40 A's)
-    "CVE": re.compile(r"^CVE-[0-9]{4}-([0-9N]{3,11}[0-9]|GH[0-9]{1,12}#[a-z0-9+.-]{1,40})$"),
-    # date only: YYYY-MM-DD
-    # date and time: YYYY-MM-DD HH:MM:SS
-    # date and time with timezone: YYYY-MM-DD HH:MM:SS TZ|+-N
-    "date-only": re.compile(r"20[0-9][0-9]-[01][0-9]-[0-3][0-9]$"),
-    "date-time": re.compile(
-        r"20[0-9][0-9]-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]$"
-    ),
-    "date-full-offset": re.compile(
-        r"20[0-9][0-9]-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] [+-][01][0-9]+$"
-    ),
-    "date-full-tz": re.compile(
-        r"20[0-9][0-9]-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] [A-Z]+$"
-    ),
-    "date-full": re.compile(
-        r"20[0-9][0-9]-[01][0-9]-[0-3][0-9](| [0-2][0-9]:[0-5][0-9]:[0-5][0-9](| ([+-][01][0-9]+|[A-Z]+)))$"
-    ),
-}
+def readConfig():
+    """Read configuration for the tools"""
+    config = configparser.ConfigParser()
+    if os.path.exists(configFilePath):
+        config.read(configFilePath)
+    else:
+        config["Locations"] = {
+            "cve-data": "/set/to/path/for/influx-security-tools-cve-data",
+        }
+        orig = os.umask(0o027)
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as f:
+            config.write(f)
+            f.flush()
+            shutil.copyfile(f.name, configFilePath, follow_symlinks=False)
+            os.umask(orig)
+        msg("Created default config in %s" % configFilePath)
+
+    return config
+
+
+def getConfigCveDataPath():
+    config = readConfig()
+    if "Locations" in config and "cve-data" in config["Locations"]:
+        path = config["Locations"]["cve-data"]
+        if not os.path.isdir(path):
+            error(
+                "Please configure %s to\nset 'cve-data' in "
+                "'[Locations]' to a valid path" % configFilePath
+            )
+
+        return config["Locations"]["cve-data"]
 
 
 #
