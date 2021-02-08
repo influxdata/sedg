ca633044eb4d90b9d09ce795eaa978f270af292e
Jamie Strandboge <jamie@influxdata.com>
2021-02-08 10:03:58 -0600

    add config file handling tests for cvelib/common.py

--- cvelib/common.py
+++ cvelib/common.py
@@ -14,15 +14,14 @@ from email.parser import BytesHeaderParser
 from email.policy import default
 
 
-# Our config file
-configFilePath = os.path.expandvars("$HOME/.config/influx-security-tools.conf")
-
 # Compile common regex on import
 rePatterns = {
     # CVE-YYYY-XXXX (1-12 X's)
     # CVE-YYYY-NNNX (1-11 N's)
     # CVE-YYYY-GHXXXX#AAAA (1-12 X's, 1-40 A's)
-    "CVE": re.compile(r"^CVE-[0-9]{4}-([0-9N]{3,11}[0-9]|GH[0-9]{1,12}#[a-z0-9+.-]{1,40})$"),
+    "CVE": re.compile(
+        r"^CVE-[0-9]{4}-([0-9N]{3,11}[0-9]|GH[0-9]{1,12}#[a-z0-9+.-]{1,40})$"
+    ),
     # date only: YYYY-MM-DD
     # date and time: YYYY-MM-DD HH:MM:SS
     # date and time with timezone: YYYY-MM-DD HH:MM:SS TZ|+-N
@@ -72,18 +71,59 @@ def error(out, exitCode=1, do_exit=True):
         sys.exit(exitCode)
 
 
+def recursive_rm(dirPath, contents_only=False, top=True):
+    """recursively remove directory"""
+    if top:
+        os.chmod(dirPath, 0o0755)  # ensure the top dir is always removable
+
+    try:
+        names = os.listdir(dirPath)
+    except PermissionError:
+        # If directory has weird permissions (eg, 000), just try to remove the
+        # directory if we can. If it is non-empty, we'll legitimately fail
+        # here. This allows us to remove empty directories with weird
+        # permissions.
+        os.rmdir(dirPath)
+        return
+
+    for name in names:
+        path = os.path.join(dirPath, name)
+        if os.path.islink(path) or not os.path.isdir(path):
+            os.unlink(path)
+        else:
+            try:
+                recursive_rm(path, top=False)
+            except PermissionError:
+                os.chmod(path, 0o0755)  # LP: #1712476
+                recursive_rm(path, top=False)
+
+    if contents_only is False:
+        os.rmdir(dirPath)
+
+
 def readCveHeaders(fn):
     """Read CVE data from file"""
     with open(fn, "rb") as fp:
         return BytesHeaderParser(policy=default).parse(fp)
 
 
+def getConfigFilePath():
+    """Return the path to influx-security-tools.conf"""
+    if "XDG_CONFIG_HOME" in os.environ:
+        return os.path.expandvars("$XDG_CONFIG_HOME/influx-security-tools.conf")
+    return os.path.expandvars("$HOME/.config/influx-security-tools.conf")
+
+
 def readConfig():
     """Read configuration for the tools"""
     config = configparser.ConfigParser()
+    configFilePath = getConfigFilePath()
     if os.path.exists(configFilePath):
         config.read(configFilePath)
     else:
+        parent = os.path.dirname(configFilePath)
+        if not os.path.isdir(parent):
+            os.mkdir(parent, 0o0700)
         config["Locations"] = {
             "cve-data": "/set/to/path/for/influx-security-tools-cve-data",
         }
@@ -92,14 +132,15 @@ def readConfig():
             config.write(f)
             f.flush()
             shutil.copyfile(f.name, configFilePath, follow_symlinks=False)
+            os.unlink(f.name)
             os.umask(orig)
         msg("Created default config in %s" % configFilePath)
 
-    return config
+    return (config, configFilePath)
 
 
 def getConfigCveDataPath():
-    config = readConfig()
+    (config, configFilePath) = readConfig()
     if "Locations" in config and "cve-data" in config["Locations"]:
         path = config["Locations"]["cve-data"]
         if not os.path.isdir(path):
--- cvelib/tests/common_test.py
+++ cvelib/tests/common_test.py
@@ -1,11 +1,37 @@
 """test_common.py: tests for common.py module"""
 
+import os
 from unittest import TestCase
+import tempfile
+
 import cvelib.common
 
 
 class TestCommon(TestCase):
     """Tests for common functions"""
+
+    def setUp(self):
+        """Setup functions common for all tests"""
+        self.orig_xdg_config_home = None
+        self.tmpdir = None
+
+    def tearDown(self):
+        """Teardown functions common for all tests"""
+        if self.orig_xdg_config_home is None:
+            if "XDG_CONFIG_HOME" in os.environ:
+                del os.environ["XDG_CONFIG_HOME"]
+        else:
+            os.environ["XDG_CONFIG_HOME"] = self.orig_xdg_config_home
+            self.orig_xdg_config_home = None
+
+        if self.tmpdir is not None:
+            cvelib.common.recursive_rm(self.tmpdir)
+
+    def _createTmpDir(self):
+        """Create a temporary directory"""
+        d = tempfile.mkdtemp(prefix="influx-security-tools-")
+        return d
+
     def test_msg(self):
         """Test msg()"""
         cvelib.common.msg("Test msg")
@@ -17,3 +43,56 @@ class TestCommon(TestCase):
     def test_error(self):
         """Test error()"""
         cvelib.common.error("Test error", do_exit=False)
+
+    def test_getConfigFilePath(self):
+        """Test getConfigFilePath()"""
+        if "XDG_CONFIG_HOME" in os.environ:
+            self.orig_xdg_config_home = os.environ["XDG_CONFIG_HOME"]
+
+        os.environ["XDG_CONFIG_HOME"] = "/fake/.config"
+        res = cvelib.common.getConfigFilePath()
+        self.assertEqual(res, "/fake/.config/influx-security-tools.conf")
+
+        del os.environ["XDG_CONFIG_HOME"]
+        res = cvelib.common.getConfigFilePath()
+        self.assertEqual(
+            res, os.path.expandvars("$HOME/.config/influx-security-tools.conf")
+        )
+
+    def test_readConfig(self):
+        """Test readConfig()"""
+        self.tmpdir = self._createTmpDir()
+
+        if "XDG_CONFIG_HOME" in os.environ:
+            self.orig_xdg_config_home = os.environ["XDG_CONFIG_HOME"]
+
+        os.environ["XDG_CONFIG_HOME"] = os.path.join(self.tmpdir, ".config")
+        fn = os.path.expandvars("$XDG_CONFIG_HOME/influx-security-tools.conf")
+
+        (exp_conf, exp_fn) = cvelib.common.readConfig()
+        self.assertEqual(fn, exp_fn)
+        self.assertTrue("Locations" in exp_conf)
+
+    def test_getConfigCveDataPath(self):
+        """Test getConfigCveDataPath()"""
+        self.tmpdir = self._createTmpDir()
+
+        if "XDG_CONFIG_HOME" in os.environ:
+            self.orig_xdg_config_home = os.environ["XDG_CONFIG_HOME"]
+
+        os.environ["XDG_CONFIG_HOME"] = os.path.join(self.tmpdir, ".config")
+        os.mkdir(os.environ["XDG_CONFIG_HOME"], 0o0700)
+        data = os.path.join(os.environ["XDG_CONFIG_HOME"], "data")
+        os.mkdir(data, 0o0700)
+        fn = os.path.expandvars("$XDG_CONFIG_HOME/influx-security-tools.conf")
+
+        with open(fn, "w") as fp:
+            fp.write(
+                """[Locations]
+cve-data = %s
+"""
+                % data
+            )
+
+        exp_fn = cvelib.common.getConfigCveDataPath()
+        self.assertEqual(exp_fn, data)
