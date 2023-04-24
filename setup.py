c3f56c426252eedad52fef67a12affaa65533026
Jamie Strandboge <jamie@influxdata.com>
2023-04-24 17:14:57 -0500

    chore: update setup.py to install ./bin/*

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
 from setuptools import setup
 if __name__ == "__main__":
-    setup()
+    # TODO: console_scripts for python scripts
+    setup(
+        scripts=[
+            # bash
+            "bin/cve-edit",
+            "bin/cve-nfu",
+            # python
+            "bin/cve-add",
+            "bin/cve-check-syntax",
+            "bin/cve-report",
+            "bin/cve-report-updated-bugs",
+            "bin/gar-report",
+            "bin/gh-report",
+            "bin/quay-report",
+        ])
