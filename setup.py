c42e2fec82810e06ab018032a8a441c49fa6b461
Jamie Strandboge <jamie@influxdata.com>
2023-04-25 08:15:08 -0500

    chore: use console_scripts for all python in bin/

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
 from setuptools import setup
 if __name__ == "__main__":
-    # TODO: console_scripts for python scripts
     setup(
         scripts=[
             # bash
             "bin/cve-edit",
             "bin/cve-nfu",
-            # python
-            "bin/cve-add",
-            "bin/cve-check-syntax",
-            "bin/cve-report",
-            "bin/cve-report-updated-bugs",
-            "bin/gar-report",
-            "bin/gh-report",
-            "bin/quay-report",
         ])
