9bc280dc14323e41caf02790a35052fff2803bc8
Jamie Strandboge <jamie@influxdata.com>
2023-05-31 12:50:44 -0500

    feat: add experimental gar-dump-reports

--- cvelib/__init__.py
+++ cvelib/__init__.py
@@ -6,6 +6,7 @@ from cvelib.cve import (
     main_cve_add,
     main_cve_check_syntax,
 )
+from cvelib.gar import main_dump_reports
 from cvelib.github import main_dump_alerts
 from cvelib.report import main_report
 
@@ -13,4 +14,5 @@ from cvelib.report import main_report
 cve_add = main_cve_add
 cve_check_syntax = main_cve_check_syntax
 cve_report = main_report
+gar_dump_reports = main_dump_reports
 gh_dump_alerts = main_dump_alerts
