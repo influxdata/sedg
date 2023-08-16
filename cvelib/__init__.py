14aa0ae6a5fc078ca3fa52a0af3ef00e2c9569bc
Jamie Strandboge <jamie@influxdata.com>
2023-08-16 14:53:21 -0500

    feat: add dso.docker.com support

--- cvelib/__init__.py
+++ cvelib/__init__.py
@@ -6,6 +6,7 @@ from cvelib.cve import (
     main_cve_add,
     main_cve_check_syntax,
 )
+from cvelib.dso import main_dso_dump_reports
 from cvelib.gar import main_gar_dump_reports
 from cvelib.github import main_dump_alerts
 from cvelib.quay import main_quay_dump_reports
@@ -15,6 +16,7 @@ from cvelib.report import main_report
 cve_add = main_cve_add
 cve_check_syntax = main_cve_check_syntax
 cve_report = main_report
+dso_dump_reports = main_dso_dump_reports
 gar_dump_reports = main_gar_dump_reports
 gh_dump_alerts = main_dump_alerts
 quay_dump_reports = main_quay_dump_reports
