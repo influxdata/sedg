16b1420a8e743c04b1d9fbe71af7c63694b09cd4
Jamie Strandboge <jamie@influxdata.com>
2026-03-24 13:39:00 -0500

    fix: black for setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
+
 if __name__ == "__main__":
     setup(
         scripts=[
             # bash
             "bin/cve-edit",
             "bin/cve-nfu",
-        ])
+        ]
+    )
